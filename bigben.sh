403632bcf5d4194d509fa54390a5e63494dea623
Paul Shiryaev <pshiryaev3@gatech.edu>
2024-05-22 22:54:34 -0400

    Added support for OSX

--- bigben.sh
+++ bigben.sh
@@ -8,23 +8,37 @@ cd "$(dirname "$0")"
 hour=$(date +"%I")
 minute=$(date +"%M")
 
-# play chimes
-if [[ $minute == 00 ]] 
-	then
-  		aplay wav/bigben0.wav
-		i=0
-		while [ $i -lt $hour ]
-		do
-			aplay wav/bigbenstrike.wav
-		    let i=i+1
-		done
-elif [[ $minute == 15 ]] 
-	then 
-		aplay wav/bigben15.wav
-elif [[ $minute == 30 ]] 
-	then
-  		aplay wav/bigben30.wav
-elif [[ $minute == 45 ]] 
-	then
-  		aplay wav/bigben45.wav
+# get platform
+if [ $(uname) == "Darwin" ]; then
+	aplay="afplay" # OSX
+else
+	aplay="aplay" # Debian default
 fi
+
+# play chimes
+case $minute in
+
+    "00")
+	$aplay wav/bigben0.wav
+	i=0
+	while [ $i -lt $hour ]
+	do
+	    aplay wav/bigbenstrike.wav
+	    let i=i+1
+	done
+	;;
+
+    "15")
+	$aplay wav/bigben15.wav
+	;;
+
+    "30")
+	$aplay wav/bigben30.wav
+	;;
+
+    "45")
+	$aplay wav/bigben45.wav
+	;;
+
+esac
+
